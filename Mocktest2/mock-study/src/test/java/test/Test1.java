fd01fcddc3141ebd2f8ecdd961e5f65c0194ea5f
Steven <littleguan@163.com>
2015-08-12 23:37:06 +0800

    Good demo for FitNesse

--- Mocktest2/mock-study/src/test/java/test/Test1.java
+++ Mocktest2/mock-study/src/test/java/test/Test1.java
@@ -1,26 +1,49 @@
 package test;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import bean.TBean;
+import bean.Target;
 import mockit.Expectations;
 import mockit.Mocked;
+import mockit.Verifications;
 
 public class Test1 {
 	@Mocked  TBean bean;
+
+
+    private Target target = new Target();
+    
+    @BeforeClass
+    public static void befor(){
+    	
+    }
+    
 
     @Test
     public void test1(){
 		
 		new Expectations() {
 			{
-			    bean.getName();returns("A","B","C","D");
+			    bean.getName();returns("A","B");
+			    //times=4;
+			    bean.getId();returns("1","2","3","4",null);
+			    //			    times=2;
+			    
+			   
 			}
 		};
-		
-		System.out.println(bean.getName());
-		
-		System.out.println(bean.getName());
+
+		target.test1();
+		target.test1();
+		target.test1();
+		new Verifications(){
+		    {
+			System.out.println("Verification:" + bean.getId());
+		    }
+		};
+
 		
 	}
 
